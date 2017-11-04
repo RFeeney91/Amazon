600d8a44fb00537b8c6d4e2866386c2bfe597986
RFeeney91 <ryansfeeney@gmail.com>
2017-11-04 06:07:54 -0700

    Add files via upload

---
+++ seed.sql
@@ -0,0 +1,20 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("iPod", "Electonics", 99, 3);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Cake", "Food", 5, 10);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Futon", "Furniture", 1000, 1);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Tires", "Automotive", 20, 12);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Forks", "Kitchen", 6, 4);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Dog Food", "Pets", 8, 5);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Asprin", "Pharmacy", 10, 15);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Lady Bugs", "Gardening", 4, 2);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("The Bible", "Literature", 5, 1000);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("T-Shirt", "Clothing", 6, 10);
